f77880d359ecde8de32063961e1a1f46759bd90c
sunlight0928 <info.tw91926@gmail.com>
2025-01-21 15:18:09 -0500

    fix job backend

--- frontend/src/app/api/chat/route.ts
+++ frontend/src/app/api/chat/route.ts
@@ -1,11 +1,11 @@
 import { Message } from "@/app/types";
 import { OpenAIStream } from "@/app/utils";
 
-export const runtime = 'edge';
+export const config = { runtime : "edge" }; // Use this instead of `export const config`
 
-export const POST =  async (req: Request): Promise<Response> => {
+export async function POST(request: Request) {
   try {
-    const { messages } = (await req.json()) as {
+    const { messages } = (await request.json()) as {
       messages: Message[];
     };
 
@@ -29,5 +29,4 @@ export const POST =  async (req: Request): Promise<Response> => {
     console.error(error);
     return new Response("Error", { status: 500 });
   }
-};
-
+}
