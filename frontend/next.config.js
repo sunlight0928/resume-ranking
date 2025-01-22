3ef1d99d86ff356f127ea74514ed5209c2463c82
sunlight0928 <info.tw91926@gmail.com>
2025-01-21 19:20:23 -0500

    fix job detail error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     domains: ["https://lh3.googleusercontent.com", "*"],
   },
   // experimental: {
