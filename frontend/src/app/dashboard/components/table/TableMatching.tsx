c0b969b716090fa362a3f406ed0259edc30364bb
sunlight0928 <info.tw91926@gmail.com>
2025-02-07 04:18:00 -0500

    fix ui 1

--- frontend/src/app/dashboard/components/table/TableMatching.tsx
+++ frontend/src/app/dashboard/components/table/TableMatching.tsx
@@ -911,7 +911,7 @@ const TableFAQ = (props: Props) => {
           className={`flex mb-4 px-3 py-2 text-sm font-medium text-center text-white rounded-lg focus:outline-none bg-[#7059F3] hover:bg-[#7059F3AA] ${
             loadingMatching
               ? "bg-gray-300 cursor-not-allowed"
-              : "bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              : "bg-[#7059F3] hover:bg-[#7059F3AA] focus:ring-4 focus:ring-blue-300 dark:bg-[#7059F3] dark:hover:bg-[#7059F3AA] dark:focus:ring-blue-800"
           }`}
           onClick={() => handleMatchingCandidate()}
           disabled={loadingMatching}
